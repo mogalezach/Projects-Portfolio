2da4b8537fcaaa02cbf9432729d2fd0086f348ff
mogalezach <mogalezach@gmail.com>
2026-03-26 21:40:51 +0200

    Add files via upload

---
+++ home/pages/frontscore.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 21:38:36 2026
+
+@author: Mogale
+"""
+
+import streamlit as st
+
+st.title("FrontScore")
+
+st.write("This is the FrontScore project page.")
